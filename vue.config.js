eebcfd25739a279be450f62ad9d30dd7eeed288a
Cheng Huang <chenghuang@ddmarketinghub.com>
2021-04-14 17:06:47 +0800

    update ip

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/local': {
-        target: 'http://18.167.121.200:8680',
+        target: 'http://18.162.151.236:8680',
         pathRewrite: { '^/local': '' },
         ws: false,
         secure: false,
